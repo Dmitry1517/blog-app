b2bb5f43c236663b62491dd7862e50aba78e2bd7
Dmitry Ivannikov <postbox404@gmail.com>
2024-03-09 16:31:19 +1000

    readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { Offline } from 'react-detect-offline';
+import { Alert } from 'antd';
+import { WifiOutlined } from '@ant-design/icons';
+
+import List from './components/List/List';
+import Article from './components/Article/Article';
+import SignUp from './components/SignUp/SignUp';
+import SignIn from './components/SignIn/SignIn';
+import DefendedWrapper from './components/MyHoc/DefenedWrap';
+import Header from './components/Header/Header';
+import { selectIsCurentUserLoading, getCurrentUser } from './redux/store/userSlice';
+import EditForm from './components/EditForm/EditForm';
+import ArticleForm from './components/ArticleForm/ArticleForm';
+import styles from './App.module.scss';
 
 function App() {
+  const isCurrentUserLoading = useSelector(selectIsCurentUserLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, []);
+
+  if (isCurrentUserLoading) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Offline>
+        <Alert
+          message="Offline"
+          description="you have to fix internet problems"
+          type="error"
+          showIcon
+          icon={<WifiOutlined />}
+          className={styles.offlineAlert}
+        />
+      </Offline>
+      <Header />
+      <Routes>
+        <Route path="/" element={<List />} />
+        <Route path="/articles" element={<List />} />
+        <Route path="/articles/:slug" element={<Article />} />
+        <Route element={<DefendedWrapper auth />}>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Route>
+        <Route element={<DefendedWrapper redirectPath="/sign-in" />}>
+          <Route path="/profile" element={<EditForm />} />
+          <Route path="/new-article" element={<ArticleForm />} />
+          <Route path="/articles/:slug/edit" element={<ArticleForm editMode />} />
+        </Route>
+        <Route path="*" element={<h2>Page doesnt exist</h2>} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import articleListReducer from './redux/store/articleListSlice';
+import articleSliceReducer from './redux/store/articleSlice';
+import userSliceReducer from './redux/store/userSlice';
+
+const store = configureStore({
+  reducer: {
+    articleList: articleListReducer,
+    article: articleSliceReducer,
+    account: userSliceReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
